a4e992125ce962f769b28f05790cfb660c1cbd71
Nico <nico@roombeast.com>
2023-10-22 01:20:24 -0300

    '1.13'

--- src/index.js
+++ src/index.js
@@ -10,27 +10,70 @@ const anecdotes = [
   "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
 ];
 
-const anecdota = (selected) => {
-  return <div>{anecdotes[selected]}</div>;
+const Anecdota = ({ selected }) => {
+  // console.log(anecdotes[selected]);
+  return <div>{anecdotes[selected]} </div>;
 };
 
-const button = (selected, setSelected) => {
-  // const [selected, setSelected] = useState(0);
+const vote = (selected, points) => {
+  //console.log("click");
+  const copy = [...points];
+  copy[selected]++;
+  return copy; // Devuelve el nuevo array con la puntuación actualizada.
+};
 
+const TextVote = ({ selected, points }) => {
+  return <div>Has {points[selected]} votes</div>;
+};
+
+const ButtonVote = ({ selected, points, setPoints }) => {
+  // console.log("entro");
   return (
     <div>
-      <button onClick={() => setSelected(selected + 1)}>next anecdote</button>
+      <button onClick={() => setPoints(vote(selected, points))}>Vote</button>
     </div>
+  );
+};
+
+const ButtonPrev = ({ selected, setSelected }) => {
+  return (
+    <>
+      <button
+        disabled={selected <= 0}
+        onClick={() => setSelected(selected - 1)}
+      >
+        previous anecdote
+      </button>
+    </>
+  );
+};
+
+const ButtonNext = ({ selected, setSelected }) => {
+  return (
+    <>
+      <button
+        disabled={selected >= anecdotes.length - 1}
+        onClick={() => setSelected(selected + 1)}
+      >
+        next anecdote
+      </button>
+    </>
   );
 };
 
 const App = (props) => {
   const [selected, setSelected] = useState(0);
+  const [points, setPoints] = useState(new Array(anecdotes.length).fill(0));
 
   return (
     <div>
-      {anecdota(selected)}
-      {button(selected, setSelected)}
+      <Anecdota selected={selected} />
+      <TextVote selected={selected} points={points} />
+      <ButtonVote selected={selected} points={points} setPoints={setPoints} />
+      <div>
+        <ButtonPrev selected={selected} setSelected={setSelected} />
+        <ButtonNext selected={selected} setSelected={setSelected} />
+      </div>
     </div>
   );
 };
